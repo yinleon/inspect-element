d09554959fa17b87b3b90124a9824e341acb52fc
leon yin <leon.yin@themarkup.org>
2023-02-13 00:46:49 -0500

    fixed buttons

--- utils.py
+++ utils.py
@@ -6,8 +6,8 @@ def build_buttons(link, github):
     Used to generate github, collab, and website-linked buttons
     """
     display(Markdown(f"""
-<button type="button" class="btn btn-outline-primary btn-sm" target="_blank" href="{link}">📖 Read online</button>
-<button type="button" class="btn btn-outline-primary btn-sm" target="_blank" href="https://colab.research.google.com/github/{github.split(".com/")[-1]}">🖥️ Interactive version</button>
-<button type="button" class="btn btn-outline-primary btn-sm" target="_blank" href="{github}">⚙️ GitHub</button>
+<a type="button" class="btn btn-outline-primary btn-sm" href="https://inspectelement.org/{link}">📖 Read online</a>
+<a type="button" class="btn btn-outline-primary btn-sm" href="https://colab.research.google.com/github/{github.split(".com/")[-1]}">🖥️ Interactive version</a>
+<a type="button" class="btn btn-outline-primary btn-sm" href="{github}">⚙️ GitHub</a>
 <br>
 """))
